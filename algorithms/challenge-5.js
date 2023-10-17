41be78ca6843a660a3d062c2f1819d5eaf0e822f
Matthew Idungafa <mattidungafa@gmail.com>
2023-10-17 08:59:18 +0100

    updated the styling and commented  the html

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -14,19 +14,3 @@ function classifyTemperature(temp) {
 console.log(classifyTemperature(5));
 module.exports = classifyTemperature;
 
-/*
-# Challenge 5
-
-Implement a function that classifies a temperature as "Freezing ( below 0) ," 
-"Cold (between 0 and 10)," "Mild (between 10 and 20)," "Warm (between 20 and 30)," 
-or "Hot (above 30)" based on a given temperature value.
-
-# Example
-
-classifyTemperature(0) should return "Freezing"
-classifyTemperature(7) should return "Cold".
-classifyTemperature(12) should return "Mild".
-classifyTemperature(23) should return "Warm"
-classifyTemperature(83) should return "Hot"
-
-*/
